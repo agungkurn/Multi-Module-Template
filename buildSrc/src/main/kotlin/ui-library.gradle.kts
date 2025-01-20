94610fa6429f6f4a78066ac55bce0f508bc65b1f
agung <agung.excurive@gmail.com>
2025-01-20 10:23:13 +0700

    separate android & non-android configs

--- buildSrc/src/main/kotlin/ui-library.gradle.kts
+++ buildSrc/src/main/kotlin/ui-library.gradle.kts
@@ -4,26 +4,71 @@ import org.gradle.kotlin.dsl.plugins
 import org.gradle.kotlin.dsl.the
 
 plugins {
-    id("common-library")
+    id("com.android.library")
+    id("org.jetbrains.kotlin.android")
+    id("com.google.dagger.hilt.android")
     id("org.jetbrains.kotlin.plugin.compose")
+    kotlin("kapt")
 }
 
 val libs = the<LibrariesForLibs>()
 
 android {
+    compileSdk = 35
+
+    defaultConfig {
+        minSdk = 28
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        consumerProguardFiles("consumer-rules.pro")
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+
+    kotlinOptions {
+        jvmTarget = "11"
+    }
+
     buildFeatures {
+        buildConfig = true
         compose = true
     }
 }
 
-dependencies{
+kotlin{
+    sourceSets.all {
+        languageSettings.enableLanguageFeature("ExplicitBackingFields")
+    }
+}
+
+hilt {
+    enableAggregatingTask = true
+}
+
+dependencies {
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.kotlinx.coroutines.core)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
     implementation(libs.androidx.viewmodel)
     implementation(libs.bundles.coil)
     implementation(platform(libs.androidx.compose.bom))
     implementation(libs.bundles.compose)
-    testImplementation(libs.junit)
+    implementation(libs.hilt)
+    kapt(libs.hilt.compiler)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
     androidTestImplementation(platform(libs.androidx.compose.bom))
--- common/build.gradle.kts
+++ common/build.gradle.kts
@@ -3,7 +3,3 @@ import org.gradle.kotlin.dsl.libs
 plugins {
     id(libs.plugins.common.library.get().pluginId)
 }
-
-android {
-    namespace = "id.ak.multimoduletemplate.common"
-}
